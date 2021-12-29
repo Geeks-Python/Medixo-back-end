480c520335914e5cd7bdfee1301044f3408fb9d5
yousef-alramli <ysabdys1996@gmail.com>
2021-12-29 16:46:21 +0200

    pharmacies

--- Pharmacy/models.py
+++ Pharmacy/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
-# Create your models here.
 
 class Pharmacy(models.Model):
+    name = models.CharField(max_length=20, blank=True, null=True)
     email = models.EmailField(unique=True, max_length=20, verbose_name="Email Address")
-    country = models.CharField(max_length=20, blank=True, null=True)
-    age = models.IntegerField(blank=True, null=True) 
+    phone_number = models.CharField(unique=True, max_length=14,null=True,blank=True)
+    city = models.CharField(max_length=20, blank=True, null=True)
+    town = models.CharField(max_length=20, blank=True, null=True)
+    building_number = models.CharField(max_length=20, blank=True, null=True)
+    street = models.CharField(max_length=20, blank=True, null=True)
+    
+
+
+
+    def __str__(self):
+        return self.name
