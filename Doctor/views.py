bd79aea92a52350aa3fc88b07db441a25507c878
yousef-alramli <ysabdys1996@gmail.com>
2021-12-30 19:40:08 +0200

    back end first version

--- Doctor/views.py
+++ Doctor/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
+from .serializers import DoctorSerializer
+from .models import Doctor
+from .permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+
+class DoctorList(ListCreateAPIView):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+class DoctorDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
