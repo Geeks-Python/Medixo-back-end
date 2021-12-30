1df2fb98e9883e8af670255f005d639c1ec4d24d
yousef-alramli <ysabdys1996@gmail.com>
2021-12-30 19:46:49 +0200

    first back-end version

--- Doctor/models.py
+++ Doctor/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth import get_user_model
 
 
 class Doctor(models.Model):
-<<<<<<< HEAD
     name = models.CharField(max_length=20, blank=True, null=True)
     email = models.EmailField(unique=True, max_length=30, verbose_name="Email Address")
     phone_number = models.CharField(unique=True, max_length=14,null=True,blank=True)
@@ -12,41 +11,10 @@ class Doctor(models.Model):
     building_number = models.CharField(max_length=20, blank=True, null=True)
     street = models.CharField(max_length=20, blank=True, null=True)
     user = models.ForeignKey(get_user_model() , on_delete = models.CASCADE) 
-
     
-=======
 
-
-    gender_category=(
-        ('Male','Male'),
-        ('Female','Female'),
-    )
-
-    category=(
-        ('Anesthesiology','Anesthesiology'),
-        ('Neurology','Neurology'),
-        ('Pathology','Pathology'),
-        ('Surgery','Surgery'),
-    )
-
-    name = models.CharField(max_length=20, blank=True, null=True)
-    email = models.EmailField(unique=True, max_length=20, verbose_name="Email Address")
-    phone_number = models.CharField(unique=True, max_length=14,null=True,blank=True)
-    city = models.CharField(max_length=20, blank=True, null=True)
-    gender=models.CharField(max_length=100,null=True,choices=gender_category)
-
-    specialty=models.CharField(max_length=100,null=True,choices=category)
-    town = models.CharField(max_length=20, blank=True, null=True)
-    building_number = models.CharField(max_length=20, blank=True, null=True)
-    street = models.CharField(max_length=20, blank=True, null=True)
->>>>>>> 7ba62cfdc11daecef80eb3625d30697bed52bfce
 
 
 
     def __str__(self):
-<<<<<<< HEAD
         return self.name
-=======
-        return self.name
-
->>>>>>> 7ba62cfdc11daecef80eb3625d30697bed52bfce
