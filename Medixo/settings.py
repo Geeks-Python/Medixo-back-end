e2ca9f646dad4b7b757db47c88382a74d4a09d4e
yousef-alramli <ysabdys1996@gmail.com>
2021-12-31 22:15:41 +0200

    final back-end

--- Medixo/settings.py
+++ Medixo/settings.py
@@ -41,10 +41,12 @@ INSTALLED_APPS = [
     'Doctor',
     'Pharmacy',
     
+    'corsheaders',
     "rest_framework",
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +57,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'Medixo.urls'
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
