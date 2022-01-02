623c9fd181a8096759b2b191e1039c2f16e4e279
salsabilmislat <salsabilmislat196@gmail.com>
2022-01-02 21:51:14 +0200

    new edit

--- Doctor/urls.py
+++ Doctor/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import DoctorList, DoctorDetail
+from .views import DoctorList, DoctorDetail, AppointmentDetail, AppointmentList
 
 urlpatterns = [
     path("", DoctorList.as_view(), name="Doctor_list"),
     path("<int:pk>/", DoctorDetail.as_view(), name="Doctor_detail"),
+    path("appointment/", AppointmentList.as_view(), name="Appointment_list"),
+    path("appointment/<int:pk>/", AppointmentDetail.as_view(), name="Appointment_detail"),
 ]
--- Doctor/views.py
+++ Doctor/views.py
@@ -3,8 +3,9 @@ from rest_framework.generics import (
     ListCreateAPIView,
     RetrieveUpdateDestroyAPIView,
 )
-from .serializers import DoctorSerializer
-from .models import Doctor
+from .serializers import DoctorSerializer, AppointmentSerializer
+from .models import Doctor, Appointment
+from .permissions import IsAuthenticatedOrReadOnly
 
 
 class DoctorList(ListCreateAPIView):
@@ -13,7 +14,15 @@ class DoctorList(ListCreateAPIView):
 
 class DoctorDetail(RetrieveUpdateDestroyAPIView):
     queryset = Doctor.objects.all()
+    permission_classes = IsAuthenticatedOrReadOnly
     serializer_class = DoctorSerializer
 
 
+class AppointmentList(ListCreateAPIView):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
 
+class AppointmentDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Appointment.objects.all()
+    permission_classes = IsAuthenticatedOrReadOnly
+    serializer_class = AppointmentSerializer
