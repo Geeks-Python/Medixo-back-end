482898609c799985731b73bf8c67a43d40f9306c
yousef-alramli <ysabdys1996@gmail.com>
2022-01-03 00:38:02 +0200

    making appointments

--- Doctor/models.py
+++ Doctor/models.py
@@ -14,15 +14,13 @@ class Doctor(models.Model):
     user = models.ForeignKey(get_user_model() , on_delete = models.CASCADE) 
     img = models.CharField(max_length=1000, blank=True, null=False, default="https://www.jupiterhospital.com/uploadedfiles/gallery/1584607002_male-dummy.jpg")
     opening_hours = models.JSONField(default=list, blank=True)
-    patient_name = models.CharField(max_length=20, blank=True, null=True, default="")
-    hour = models.CharField(max_length=20, blank=True, default="")
-    date = models.CharField(max_length=20, blank=True, default="")
     
     def __str__(self):
         return self.name
 
 class Appointment(models.Model):
-    user = models.ForeignKey(get_user_model() , on_delete = models.CASCADE) 
+    doctor_id = models.ForeignKey(get_user_model() , on_delete = models.CASCADE) 
+    patient_id = models.CharField(max_length=20, blank=True, null=True, default="") 
     patient_name = models.CharField(max_length=20, blank=True, null=True, default="")
     hour = models.CharField(max_length=20, blank=True, default="")
     date = models.CharField(max_length=20, blank=True, default="")
