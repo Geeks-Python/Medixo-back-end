f9d65d65fd23be332288959f7ecee0f85ee851a9
mohammedalsamki <momosamak21@gmail.com>
2022-01-05 18:49:57 +0200

    deploy

--- Medixo/wsgi.py
+++ Medixo/wsgi.py
@@ -7,10 +7,24 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Medixo.settings')
+
+# application = get_wsgi_application()
 import os
+import signal
 
+import sys
+import traceback
+
+import time
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Medixo.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Medixo.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
